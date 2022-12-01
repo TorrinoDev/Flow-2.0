ff616fbf69b4fede4c1303d0996d0af650359e48
AlexTriumf <TriumfAlex@gmail.com>
2022-12-01 12:38:56 +0100

    Coopmembership fix + notification

--- flow2/src/App.jsx
+++ flow2/src/App.jsx
@@ -1,10 +1,17 @@
 import Homepage from "./sections/homepage/Homepage"
+import { HopeProvider, NotificationsProvider } from '@hope-ui/solid'
 
 function App() {
   
   return (
     <div>
+       <HopeProvider>
+      <NotificationsProvider>
+
+      </NotificationsProvider>
       <Homepage/>
+
+      </HopeProvider>
     </div>
   );
 }
