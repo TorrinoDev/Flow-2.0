b5c34c7e80f5f6df7383597138f067397909cd52
andr12f4 <75079112+andr12f4@users.noreply.github.com>
2022-12-02 09:16:13 +0100

    show on demand

--- flow2/src/sections/AnswerDialog/Total.jsx
+++ flow2/src/sections/AnswerDialog/Total.jsx
@@ -18,26 +18,26 @@ function Total(props) {
         <Text id='CitizenShip'>CitizenShip:{storeUser.Citizenship}</Text>
         <Text id='car'>car: {userObject.Car}</Text> 
         <Text id='children'>children: {userObject.Children}</Text> 
-        <Show when={userObject.child.childOne.length>1}>
+        <Show when={userObject.child.childOne.length>=1}>
             <Text id='childOne'>childOne:{userObject.child.childOne}</Text>
         </Show>
-        <Show when={userObject.child.childTwo.length>1}>
+        <Show when={userObject.child.childTwo.length>=1}>
             <Text id='childTwo'>childTwo:{userObject.child.childTwo}</Text>
         </Show>
         <Show when={userObject.CohabitingEmail.length>1}>
             <Text id='CohabitingEmail'>CohabitingEmail:{userObject.CohabitingEmail}</Text>
         </Show>
-        <Show when={userObject.CoopMember.length>1}>
+        <Show when={userObject.CoopMember!=undefined}>
             <Text id='CoopMember'>CoopMember:{userObject.CoopMember}</Text>
         </Show>
-        <Show when={userObject.Employer.length>1}>
+        <Show when={userObject.Employer!=undefined}>
             <Text id='Employer'>Employer:{userObject.Employer}</Text>
         </Show>
         <Text id='Employment'>Employment: {userObject.Employment}</Text> 
-        <Show when={userObject.EmploymentMonth.length>1}>
+        <Show when={userObject.EmploymentMonth!=undefined}>
             <Text id='Employment Month'>Employment Month:{userObject.EmploymentMonth}</Text>
         </Show>
-        <Show when={userObject.EmploymentYear.length>1}>
+        <Show when={userObject.EmploymentYear!=undefined}>
             <Text id='Employment Year'>Employment Year:{userObject.EmploymentYear}</Text>
         </Show>
     </div>
